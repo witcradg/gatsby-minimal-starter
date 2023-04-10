ba5744f8a55a3d28917b4f6c7a95677a2efbfea4
witcradg <witcradg@gmail.com>
2023-04-10 15:32:49 -0700

    Add placeholder files

---
+++ src/components/placeholder.js
@@ -0,0 +1,21 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
+
+import * as React from "react"
+
+import React from 'react'
+
+export const Placeholder = () => {
+    return (
+        <div>
+            Nothing to see here.
+        </div>
+    )
+}
+
+
+export default Layout
